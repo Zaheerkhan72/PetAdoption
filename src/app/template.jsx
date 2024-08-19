0113487aa552733dc3de8bbff80450cdeec7df2a
zaheer khan <zakkhan5525@gmail.com>
2024-08-19 11:52:07 +0530

    setup

---
+++ src/app/template.jsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import { AppProvider } from "@/context/AppContext";
+import React from "react";
+
+const Template = ({ children }) => {
+  return (
+    <AppProvider>
+      <Navbar />
+      {children}
+    </AppProvider>
+  );
+};
+
+export default Template;
